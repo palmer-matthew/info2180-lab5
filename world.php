53b4a70aedc924b643885d95c5f9221765d963c3
Matthew Palmer <palmer.matthew4567@gmail.com>
2020-11-16 14:07:57 -0500

    3.2 modified world.php

--- world.php
+++ world.php
@@ -1,17 +1,35 @@
 <?php
-$host = 'localhost';
-$username = 'lab5_user';
-$password = 'password123';
-$dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+  $host = 'localhost';
+  $username = 'lab5_user';
+  $password = 'password123';
+  $dbname = 'world';
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  if($_SERVER['REQUEST_METHOD'] == 'GET'){
+      $country = filter_var($_GET['country'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+      $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+      if($country == ""){
+          $stmt = $conn->query("SELECT * FROM countries");
+          $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          $intial = "<ul>";
+          $end = "</ul>";
+          foreach ($results as $row){
+            $intial .= "<li>".$row['name'] . ' is ruled by ' . $row['head_of_state']."</li>";
+          }
+          $intial .= $end;
+          echo $intial;
+      }else{
+          $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+          $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          $intial = "<ul>";
+          $end = "</ul>";
+          foreach ($results as $row){
+            $intial .= "<li>".$row['name'] . ' is ruled by ' . $row['head_of_state']."</li>";
+          }
+          $intial .= $end;
+          echo $intial;
+      }
+  }
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
